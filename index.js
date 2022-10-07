f6b2ce3c8487a507638bd9a99c53fd2bd25080f9
krishna kant <krishnakant12198@gmail.com>
2022-10-07 12:48:28 +0530

    commit message

---
+++ index.js
@@ -0,0 +1,8 @@
+var row = Number(prompt("Enter Row Number")); 
+ var col = Number(prompt("Enter Column Number")); 
+ for (let i = 0; i < row; i++) { 
+   for (let j = 0; j < col; j++) { 
+     document.write("1 "); 
+   } 
+   document.write("<br>"); 
+ }
